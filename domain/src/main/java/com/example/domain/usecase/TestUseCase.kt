209eef2a7d114ea864e41e2649cb9fc3b13e7fe6
JongHyunSeo <jonghyun9606@gmail.com>
2025-09-10 18:54:26 +0900

    Delete Test Files

--- domain/src/main/java/com/example/domain/usecase/TestUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.usecase
-
-import javax.inject.Inject
-
-class TestUseCase @Inject constructor() {
-    operator fun invoke(): String {
-        return "Domain Hilt Test Complete"
-    }
-}
--- presentation/src/main/java/com/example/presentation/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/MainViewModel.kt
@@ -1,17 +1,10 @@
 package com.example.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.domain.usecase.TestUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val testUseCase: TestUseCase
 ) : ViewModel() {
-    val message = testUseCase()
-
-    fun showMessage() {
-        println(message)
-    }
 }
